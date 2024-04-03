953cf16fb5acee418c11c71a724a02f02ee2dcb0
pcsilva1984 <159202782+pcsilva1984@users.noreply.github.com>
2024-04-02 21:35:34 -0300

    Update ec2-user-data.sh

--- .environment/ansible-ec2/ec2-user-data.sh
+++ .environment/ansible-ec2/ec2-user-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+echo "Install Nginx"
+sudo dnf update -y
+sudo dnf install -y nginx
+sudo systemctl start nginx.service
+sudo systemctl status nginx.service
+sudo systemctl enable nginx.service
